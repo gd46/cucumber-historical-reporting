3da2445e8f431b025cbb9fa72a1bd8ac356a154e
gd46 <gd46@njit.edu>
2017-10-27 17:48:38 -0400

    refactor(navigation) remove sb-admin scripts and use angular to control hide/show of side nav

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+	@Input() isSideNavClosed: boolean;
+	@Output() isSideNavClosedChange = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+ 	}
+
+  toggleSideNav(newValue): void {
+		this.isSideNavClosed = !this.isSideNavClosed;
+		this.isSideNavClosedChange.emit(this.isSideNavClosed);
   }
 
 }
